9c3b287125f2e467b68a111529e0ce0bf62dee80
Abdelkarim CHAMLAL <abdelkarim.chamlal@gmail.com>
2022-01-25 13:07:30 +0100

    removed a test unite

--- app/FacebookAPI.php
+++ app/FacebookAPI.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class FacebookAPI extends Model
+class FacebookAPI
 {
-    //
+    public function getAuthLink($app_id, $api_version, $callback_url, $permissions){
+        return "https://www.facebook.com/$api_version/dialog/oauth?client_id=$app_id&response_type=code&redirect_uri=$callback_url&scope=$permissions";
+    }
 }
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,7 +6,25 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public function loggedIn(Request $req){
+        $access_token = session('access_token');
+        if(isset($access_token)){
+            echo " there is  a token";
+        }else{
+            echo " there is no token";
+        }
+    }
+
+    public function logIn(Request $req){
+
+    }
+
     public function callback(Request $req){
+        $this->loggedIn($req);
         echo "hi";
+    }
+
+    public function logOut(Request $req){
+
     }
 }
